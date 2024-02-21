d952c8a422e04d17057e967afab88e292a98652c
= <151518328+GheorgheBulai@users.noreply.github.com>
2024-02-21 08:12:22 +0200

    Create Text Processor class

--- Lab1/src/main/java/cum/utm/Main.java
+++ Lab1/src/main/java/cum/utm/Main.java
@@ -1,7 +1,27 @@
 package cum.utm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String sentence ;
+        int wordLenght;
+        String myWord;
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(is);
+        try {
+            System.out.println("Itrodu propozitia: ");
+            sentence = reader.readLine();
+            System.out.println("Introdu lungimea cuvantului inlocuit");
+            wordLenght = Integer.parseInt(reader.readLine());
+            System.out.println("Introdu cuvintul care trebuie inlocuit");
+            myWord = reader.readLine();
+            TextProcessor processor = new TextProcessor(sentence,myWord,wordLenght);
+            System.out.println("Result: "+processor.getResultSentence());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
